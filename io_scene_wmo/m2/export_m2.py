e8301f669a801a10dd9b0b8ee543009b18403fc9
T1ti <alkioxbf3@gmail.com>
2021-10-07 21:27:06 +0000

    revert

--- io_scene_wmo/m2/export_m2.py
+++ io_scene_wmo/m2/export_m2.py
@@ -7,7 +7,7 @@ from ..ui import get_addon_prefs
 def export_m2(version, filepath, selected_only, fill_textures):
     addon_prefs = get_addon_prefs()
 
-    m2 = M2File(version, filepath=filepath)
+    m2 = M2File(version)
     bl_m2 = BlenderM2Scene(m2, addon_prefs)
     bl_m2.save_properties(filepath, selected_only)
     bl_m2.save_bones(selected_only)
