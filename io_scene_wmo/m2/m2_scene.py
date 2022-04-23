394fbda73e4c8c05dcc2da666de30297a9d7e776
ihm-tswow <ihm-ts@protonmail.com>
2022-04-23 10:57:55 +0200

    m2: fix default texture unit color

--- io_scene_wmo/m2/m2_scene.py
+++ io_scene_wmo/m2/m2_scene.py
@@ -2189,7 +2189,7 @@ class BlenderM2Scene:
                 bl_mode = int(material.wow_m2_material.blending_mode)
                 shader_id = int(material.wow_m2_material.shader)
                 mat_layer = int(material.wow_m2_material.layer)
-                color_id = self.color_ids[material.wow_m2_material.color] if material.wow_m2_material.color != "" else 0
+                color_id = self.color_ids[material.wow_m2_material.color] if material.wow_m2_material.color != "" else -1
                 transparency_id = self.transparency_ids[material.wow_m2_material.transparency] if material.wow_m2_material.transparency != "" else 0
 
                 self.m2.add_material_to_geoset(g_index, render_flags, bl_mode, flags, shader_id, tex_id,
